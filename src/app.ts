1ec13d54114d666af83492fc318f683326db4083
masudmolla6 <masudmolla2937@gmail.com>
2023-12-12 16:11:00 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+
+// parser/medleware
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+
+  res.send('hello world')
+})
+
+export default app
